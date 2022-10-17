60749ab3701d5704333ecff58bcefb5c5df0aece
Hrijul Chauhan <chauhanhrijul4@gmail.com>
2022-10-17 14:25:30 +0530

    idk

--- vue.config.js
+++ vue.config.js
@@ -5,16 +5,11 @@ module.exports = defineConfig({
 
 const Dotenv = require('dotenv-webpack');
 
-// this is to load env vars for this config
-require('dotenv').config({ // it puts the content to the "process.env" var. System vars are taking precedence
-  path: '.env',
-});
-// and this to pass env vars to the JS application
-const DotenvPlugin = require('webpack-dotenv-plugin');  
+
 module.exports = {
   configureWebpack: {
     plugins: [
-      new Dotenv({safe: false, systemvars: true})
+      new Dotenv()
     ]
   }
 }
